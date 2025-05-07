01a1b52fd0d6da54c966892dd9138529af37edbf
주은희 <eunhee.ju@jh-solution.net>
2025-05-07 12:40:12 +0900

    feat: Add counter button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
+'use client';
+
+import { useState } from 'react';
+
+const buttonStyle = {
+  backgroundColor: 'blue',
+  color: 'white',
+  padding: '4px 12px',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+}
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div>Hello Git!</div>
+    <>
+      <div>Hello Git!</div>
+      <div>
+        <p>Count: {count}</p>
+        <button
+          type="button"
+          style={buttonStyle}
+          onClick={handleClick}
+        >
+          Click to add count
+        </button>
+      </div>
+    </>
   );
 }
