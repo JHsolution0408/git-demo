35c562933b3f69e6ad593e8e10d9ca97c318ebbb
주은희 <eunhee.ju@jh-solution.net>
2025-05-07 15:43:21 +0900

    feat: Add new button to decrease number

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@
 import { useState } from 'react';
 
 const buttonStyle = {
+  margin: '12px 8px 0 0',
+  padding: '4px 12px',
   backgroundColor: 'blue',
   color: 'white',
-  padding: '4px 12px',
   border: 'none',
   borderRadius: '5px',
   cursor: 'pointer',
@@ -14,22 +15,42 @@ const buttonStyle = {
 export default function Home() {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
+  const addCount = () => {
     setCount(count + 1);
   };
+
+  const decreaseCount = () => {
+    if (count <= 0) {
+      alert('Count cannot be less than 0');
+      return;
+    }
+
+    setCount(count - 1);
+  };
+
 
   return (
     <>
       <div>Hello Git!</div>
       <div>
         <p>Count: {count}</p>
-        <button
-          type="button"
-          style={buttonStyle}
-          onClick={handleClick}
-        >
-          Click to add count
-        </button>
+
+        <div>
+          <button
+            type="button"
+            style={buttonStyle}
+            onClick={addCount}
+          >
+            Click to add count
+          </button>
+          <button
+            type="button"
+            style={buttonStyle}
+            onClick={decreaseCount}
+          >
+            Click to decrease count
+          </button>
+        </div>
       </div>
     </>
   );
